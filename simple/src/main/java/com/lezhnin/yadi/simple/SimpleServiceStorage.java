3974e41ce889fb09f9cd855f8b6ccf1eef4d0b29
Sergey Lezhnin <s.lezhnin@gmail.com>
2017-09-29 17:27:29 +0500

    change mapping to string in SimpleServiceStorage

--- simple/src/main/java/com/lezhnin/yadi/simple/SimpleServiceStorage.java
+++ simple/src/main/java/com/lezhnin/yadi/simple/SimpleServiceStorage.java
@@ -9,20 +9,20 @@ import javax.annotation.Nullable;
 
 public class SimpleServiceStorage implements ServiceStorage {
 
-    private Map<Class<?>, ServiceProvider<?>> storage = new LinkedHashMap<>();
+    private Map<String, ServiceProvider<?>> storage = new LinkedHashMap<>();
 
     @Nullable
     @Override
     @SuppressWarnings("unchecked")
     public <T> ServiceProvider<T> get(@Nonnull final Class<T> serviceBeanInterface) {
-        return (ServiceProvider<T>) storage.get(serviceBeanInterface);
+        return (ServiceProvider<T>) storage.get(serviceBeanInterface.getName());
     }
 
     @Nonnull
     @Override
     public <T> ServiceStorage put(@Nonnull final Class<? extends T> serviceBeanInterface,
                                   @Nonnull final ServiceProvider<? extends T> serviceProvider) {
-        storage.put(serviceBeanInterface, serviceProvider);
+        storage.put(serviceBeanInterface.getName(), serviceProvider);
         return this;
     }
 }

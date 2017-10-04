92b3fabb8127c8e5967be61951ac44137d6c4f47
Sergey Lezhnin <s.lezhnin@gmail.com>
2017-10-04 18:06:12 +0500

    make ServiceRegistry accept ServiceDefinition

--- api/src/main/java/com/lezhnin/yadi/api/ServiceRegistry.java
+++ api/src/main/java/com/lezhnin/yadi/api/ServiceRegistry.java
@@ -2,5 +2,5 @@ package com.lezhnin.yadi.api;
 
 public interface ServiceRegistry extends ServiceLocator {
 
-    ServiceRegistry register(ServiceSupplier<?> serviceSupplier);
+    ServiceRegistry register(ServiceDefinition<?> serviceDefinition);
 }
